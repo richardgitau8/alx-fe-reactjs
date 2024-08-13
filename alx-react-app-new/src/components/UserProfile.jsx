87389039668de8e83d87285ce79bb256fa8d1e07
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-08-13 11:30:36 +0300

    Update UserProfile.jsx

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const UserProfile = (props) => {
     return (
       <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-        <h2 style={{ color: 'red' }}>{props.name}</h2>
+        <h2 style={{ color: 'blue'}}>{props.name}</h2>
         <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
         <p>Bio: {props.bio}</p>
       </div>
