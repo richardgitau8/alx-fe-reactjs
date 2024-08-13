4e6f44c2a9d7805de2e122b576d7e578a3f80bcb
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-08-13 11:43:35 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,22 +1,11 @@
-import React from 'react';
-import UserProfile from './components/UserProfile';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <UserProfile 
-              name="Alice" 
-              age="25" 
-              bio="Loves hiking and photography" 
-            />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
