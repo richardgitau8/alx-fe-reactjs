506356eef5ec0368e157907068b0ac2c1b398e20
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-08-22 11:39:41 +0300

    Add files via upload

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,20 @@
+import { useParams } from 'react-router-dom';
+import { useRecipeStore } from '../recipeStore';
+
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const recipe = useRecipeStore(state =>
+    state.recipes.find(recipe => recipe.id === parseInt(id))
+  );
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
