04a79820432fa39f141e07e111729092e0b42f40
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-09-05 09:20:48 +0300

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Paths to purge unused styles
+  darkMode: 'media', // or 'class' for toggling dark mode manually
   theme: {
-    extend: {},
+    extend: {
+      // You can extend the default Tailwind CSS theme here
+    },
+  },
+  variants: {
+    extend: {
+      // Extend variants for utilities, e.g., background color, border, etc.
+      backgroundColor: ['hover', 'focus', 'active', 'group-hover'],
+    },
   },
   plugins: [],
 }
-
