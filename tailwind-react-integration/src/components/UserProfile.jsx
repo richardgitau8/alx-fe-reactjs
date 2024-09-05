443aa4772b6d8f421e571e7f124f56ae64e2ea98
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-09-05 14:01:01 +0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         alt="User"
         className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-3 sm:my-4 md:my-5">
+      <h1 className="text-lg sm:text-xl  md:text-xl text-blue-800 my-3 sm:my-4 md:my-5">
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600">
