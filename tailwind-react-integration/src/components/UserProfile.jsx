086b374b599dfa52675c8a4188865bc5380cb726
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-09-11 14:17:17 +0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:text-xl md:max-w-md md:max-w-sm mx-auto my-10 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+    <div className="user-profile bg-gray-100 sm:p-4 p-4 sm:p-6 md:p-8 max-w-xs sm:w-24, sm:h-24 sm:max-w-sm md:text-xl md:max-w-md md:max-w-sm mx-auto my-10 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
