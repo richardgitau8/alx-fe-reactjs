ef74300aebaede42ff289e50efb4246dea4c7199
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-09-20 15:04:36 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
-// Function to fetch user data from GitHub API
-const fetchUserData = async (username) => {
-  const apiUrl = `https://api.github.com/users/${username}`; // API endpoint
+const fetchAdvancedUserData = async (username, location, minRepos) => {
+  let query = '';
+  if (username) query += `${username}+`;
+  if (location) query += `location:${location}+`;
+  if (minRepos) query += `repos:>${minRepos}`;
+
+  const apiUrl = `https://api.github.com/search/users?q=${query.trim()}`;
 
   try {
-    const response = await axios.get(apiUrl);  // Make GET request to GitHub API
-    return response.data;  // Return the user data
+    const response = await axios.get(apiUrl);
+    return response.data; // Return the user data
   } catch (error) {
-    console.error('Error fetching GitHub user:', error);
-    throw error;  // Throw error so it can be handled in the component
+    console.error('Error fetching GitHub users:', error);
+    throw error;
   }
 };
 
-export default fetchUserData;
+export default fetchAdvancedUserData;
