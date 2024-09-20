d044c950d366abebe5b09221d86a84d32016cff8
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-09-20 15:03:07 +0300

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -23,24 +23,20 @@ const Search = () => {
       />
       <button onClick={handleSearch}>Search</button>
 
-      {loading ? (
-        <p>Loading...</p>
+      {loading && <p>Loading...</p>}  {/* Show loading message */}
+      
+      {users.length > 0 ? (
+        users.map(user => (
+          <div key={user.login} className="border p-4 my-2">
+            <img src={user.avatar_url} alt={user.login} className="w-12 h-12" />
+            <h2 className="text-xl">{user.login}</h2>
+            <a href={user.html_url} target="_blank" rel="noopener noreferrer" className="text-blue-500">
+              View Profile
+            </a>
+          </div>
+        ))
       ) : (
-        <div>
-          {users.length > 0 ? (
-            users.map(user => (
-              <div key={user.login} className="border p-4 my-2">
-                <img src={user.avatar_url} alt={user.login} className="w-12 h-12" />
-                <h2 className="text-xl">{user.login}</h2>
-                <a href={user.html_url} target="_blank" rel="noopener noreferrer" className="text-blue-500">
-                  View Profile
-                </a>
-              </div>
-            ))
-          ) : (
-            <p>Looks like we can't find the user.</p>
-          )}
-        </div>
+        !loading && <p>Looks like we can't find the user.</p>  // Show message if no users found
       )}
     </div>
   );
